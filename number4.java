58aa3eeffaa5c89221c20ece0dc88481ae0c28eb
Rishika6 <53297570+Rishika6@users.noreply.github.com>
2019-08-24 18:18:45 +0530

    Add files via upload

---
+++ number4.java
@@ -0,0 +1,19 @@
+package javacrashcourse;
+
+public class number4 {
+
+	public static void main(String[] args)
+	{
+		int a=10;
+		int b=20;
+		int c=15;
+		int d=30;
+		int n1,n2;
+		n1=(a>b)?a:b;
+		n2=(c>d)?c:d;
+		System.out.println((n1>n2)?n1:n2);
+		
+
+	}
+
+}
