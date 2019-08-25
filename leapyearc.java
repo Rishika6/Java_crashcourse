5ba981dd2abd9acf64b836937d4f35bc9db51681
Rishika6 <53297570+Rishika6@users.noreply.github.com>
2019-08-25 23:42:47 +0530

    Update leapyearc.java

--- leapyearc.java
+++ leapyearc.java
@@ -5,15 +5,17 @@ public class leapyearc {
 	public static void main(String[] args) 
 	{
 	   int year=1992;
-	   if((year%4==0)&&(year%100==0));
+	   if(year%400==0)
 	   {
 		   System.out.println("leap year");
 	   }
 	   
-	   else if(year%400==0)
+	   else if(year%100==0)
+		   System.out.println("leap year");
+	   else if(year%4==0)
 		   System.out.println("leap year");
 	   else
-		   System.out.println("not a leap year");
+		   System.out.println("not a leap year);
 	}
 
 }
