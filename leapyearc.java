e56f80fd27f6304cc748f2d9df2b93c657826d27
Rishika6 <53297570+Rishika6@users.noreply.github.com>
2019-08-25 17:12:51 +0530

    Add files via upload

---
+++ leapyearc.java
@@ -0,0 +1,19 @@
+package javacrashcourse;
+
+public class leapyearc {
+
+	public static void main(String[] args) 
+	{
+	   int year=1992;
+	   if((year%4==0)&&(year%100==0));
+	   {
+		   System.out.println("leap year");
+	   }
+	   
+	   else if(year%400==0)
+		   System.out.println("leap year");
+	   else
+		   System.out.println("not a leap year");
+	}
+
+}
