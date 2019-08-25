118fa4ed874e2f41670e15e76afa430c10a1d444
Rishika6 <53297570+Rishika6@users.noreply.github.com>
2019-08-25 23:38:52 +0530

    Update leapyearb.java

--- leapyearb.java
+++ leapyearb.java
@@ -8,13 +8,16 @@ public class leapyearb {
 		 if(year%4==0)
 		 {
 			 if(year%100==0)
-			 {	 
+			 {
+			      if(year%400==0)
 				 System.out.println("leap year");
+			       else
+				 System.out.println("not leap year");
+			 }	 
 			 
-			 }
-		}
-		 else
-	            System.out.println("not leap year");
-	}
-
+	               else
+	                  System.out.println(" leap year");
+	        }
+               else
+	         System.out.println("not leap year");
 }
