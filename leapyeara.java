503734fd35787ef5bb34df3fa005b8e60b2ac688
Rishika6 <53297570+Rishika6@users.noreply.github.com>
2019-08-25 23:31:34 +0530

    Update leapyeara.java

--- leapyeara.java
+++ leapyeara.java
@@ -5,7 +5,7 @@ public class leapyeara{
 	public static void main(String[] args)
 	{
 		int year=2000;
-		if((year%4==0)&&(year%100==0)||(year%400==0))
+		if((year%4==0)&&(year%100!=0)||(year%400==0))
 				{
 			         System.out.println("Leap year");
 				}
