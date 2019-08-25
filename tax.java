2ec6212f32efdeddfc79604da67a781fc862b1ba
Rishika6 <53297570+Rishika6@users.noreply.github.com>
2019-08-25 19:09:50 +0530

    Update tax.java

--- tax.java
+++ tax.java
@@ -7,19 +7,19 @@ public class tax {
       double salary=35000.00;
       double tax;
       if((salary>=0.00)&&(salary<=14999.99))
-    	  tax=salary+0.00+((15/100)*salary);
+    	  tax=0.00+((15/100)*salary);
 		System.out.println("$"+tax);
       else if((salary>=15000.00)&&(salary<=29999.99))
-    	   tax=salary+2250.00+((18/100)*salary);
+    	   tax=2250.00+((18/100)*salary);
 		System.out.println("$"+tax);
       else if((salary>=30000.00)&&(salary<=49999.99))
-    	  tax=salary+5400.00+((22/100)*salary);
+    	  tax=5400.00+((22/100)*salary);
 		System.out.println("$"+tax);
       else if((salary>=50000.00)&&(salary<=79999.99))
-    	  tax=salary+11000.00+((27/100)*salary);
+    	  tax=11000.00+((27/100)*salary);
 		System.out.println("$"+tax);
       else if((salary>=80000.00)&&(salary<=150000.00))
-    	  tax=salary+21600.00+((33/100)*salary);
+    	  tax=21600.00+((33/100)*salary);
 		System.out.println("$"+tax);
        else
        {
