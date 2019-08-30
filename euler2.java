59cb68bce6ac153808a5d65c8afbcb53a4923b9b
Rishika6 <53297570+Rishika6@users.noreply.github.com>
2019-08-30 21:59:14 +0530

    Add files via upload

---
+++ euler2.java
@@ -0,0 +1,21 @@
+public class euler2
+{
+	public static void main(String args[])
+	{
+		int n1=1,n2=2;
+		int sum=0;
+		int n3;
+		do
+		{
+			n3=n1+n2;
+			n1=n2;
+			n2=n3;
+			if(n3%2==0)
+			{
+				sum=sum+n3;
+			}
+		}while(n3<=4000000);
+		System.out.println(sum);
+	}
+
+}
