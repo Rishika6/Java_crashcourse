352da87d871652dca1cab7b8224763524af6b6c6
Rishika6 <53297570+Rishika6@users.noreply.github.com>
2019-08-30 21:58:47 +0530

    Add files via upload

---
+++ Euler06.java
@@ -0,0 +1,17 @@
+package javacrashcourse;
+
+public class Euler06 {
+
+	public static void main(String[] args)
+	{
+		int sum1,sum2=0;
+		int i;
+		i=(100*101)/2;
+		sum1=i*i;
+		sum2=(100*101*201)/6;
+		System.out.println(sum1-sum2);
+		// TODO Auto-generated method stub
+
+	}
+
+}
