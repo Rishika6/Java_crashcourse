5fac49daced982b41c7f1f047dc43a6038fad642
Rishika6 <53297570+Rishika6@users.noreply.github.com>
2019-09-01 21:47:15 +0530

    Add files via upload

---
+++ Euler4.java
@@ -0,0 +1,27 @@
+public class Euler4 {
+
+	public static void main(String[] args)
+	{
+		int i,j,t,n,r;
+		int max=0;
+		int rev=0;
+		for(i=100;i<=999;i++)
+		{
+			for(j=100;j<=i;j++)
+			{
+				n=i*j;
+				t=n;
+				while(n!=0)
+				{
+					r=n%10;
+					rev=(rev*10)+r;
+					n=n/10;
+					if(t==rev)
+                                        max=t;
+                                       
+			 
+				}
+			}
+		}System.out.println(max);
+}
+}
